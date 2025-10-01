a47e1e56cf620d6e2b068e3455fb2acde11626e1
Decse1 <148325245+Decse1@users.noreply.github.com>
2025-10-01 09:26:56 +0200

    File update and add
    Updated:
    - adatbazis_REKA.docx
    Added:
    - adatbazis_REKA.sql
    - ekdiagramm_REKA.svg

---
+++ adatbazis_REKA.sql
@@ -0,0 +1,74 @@
+CREATE TABLE `Ceg` (
+  `id` integer PRIMARY KEY,
+  `nev` varchar(100),
+  `adoszam` varchar(11),
+  `eu_adoszam` varchar(20),
+  `cim` varchar(255),
+  `email` varchar(100),
+  `telefon` varchar(50),
+  `alkalmazott` text NOT NULL
+);
+
+CREATE TABLE `Felhasznalo` (
+  `id` integer PRIMARY KEY,
+  `nev` varchar(100),
+  `jelszo` varchar(100),
+  `kategoria` integer NOT NULL,
+  `telephely_cim` varchar(255)
+);
+
+CREATE TABLE `Partnerseg` (
+  `id` integer PRIMARY KEY,
+  `elado` integer NOT NULL,
+  `vevo` integer NOT NULL,
+  `fizetesi_ido` integer,
+  `fizetesi_forma` Varchar(50)
+);
+
+CREATE TABLE `Termek` (
+  `id` integer PRIMARY KEY,
+  `tulajdonos` integer NOT NULL,
+  `nev` varchar(100) NOT NULL,
+  `cikkszam` varchar(100),
+  `mennyiseg` integer,
+  `kiszereles` varchar(10),
+  `min_vas_menny` integer,
+  `leiras` text,
+  `ar` integer,
+  `kategoria` integer NOT NULL,
+  `afa_kulcs` integer
+);
+
+CREATE TABLE `Termek_kategoria` (
+  `id` integer PRIMARY KEY,
+  `nev` varchar(100)
+);
+
+CREATE TABLE `Rendeles` (
+  `id` integer PRIMARY KEY,
+  `partnerseg` integer NOT NULL,
+  `datum` date NOT NULL,
+  `status` varchar(10) NOT NULL
+);
+
+CREATE TABLE `Rendeles_tetel` (
+  `rendeles_id` integer NOT NULL,
+  `termek_id` integer NOT NULL,
+  `mennyiseg` integer NOT NULL
+);
+
+ALTER TABLE `Felhasznalo` ADD FOREIGN KEY (`id`) REFERENCES `Ceg` (`alkalmazott`);
+
+ALTER TABLE `Termek` ADD FOREIGN KEY (`tulajdonos`) REFERENCES `Ceg` (`id`);
+
+ALTER TABLE `Partnerseg` ADD FOREIGN KEY (`elado`) REFERENCES `Ceg` (`id`);
+
+ALTER TABLE `Partnerseg` ADD FOREIGN KEY (`vevo`) REFERENCES `Ceg` (`id`);
+
+ALTER TABLE `Termek` ADD FOREIGN KEY (`kategoria`) REFERENCES `Termek_kategoria` (`id`);
+
+ALTER TABLE `Termek` ADD FOREIGN KEY (`id`) REFERENCES `Rendeles_tetel` (`termek_id`);
+
+ALTER TABLE `Rendeles_tetel` ADD FOREIGN KEY (`rendeles_id`) REFERENCES `Rendeles` (`id`);
+
+ALTER TABLE `Rendeles` ADD FOREIGN KEY (`partnerseg`) REFERENCES `Partnerseg` (`id`);
