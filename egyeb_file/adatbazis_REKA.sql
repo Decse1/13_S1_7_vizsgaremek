c97464864a233b7439066f3ea7b90067e43df7cf
Decse1 <148325245+Decse1@users.noreply.github.com>
2025-11-18 07:30:45 +0100

    Update adatbazis_REKA.sql

--- egyeb_file/adatbazis_REKA.sql
+++ egyeb_file/adatbazis_REKA.sql
@@ -2,11 +2,12 @@ CREATE TABLE `Ceg` (
   `id` integer PRIMARY KEY,
   `nev` varchar(100),
   `adoszam` varchar(11),
-  `eu_adoszam` varchar(20),
+  `euAdoszam` varchar(20),
   `cim` varchar(255),
   `email` varchar(100),
-  `telefon` varchar(50),
-  `alkalmazott` text NOT NULL
+  `telefon` varchar(15),
+  `alkalmazott` text NOT NULL,
+  `elofiz` bool
 );
 
 CREATE TABLE `Felhasznalo` (
@@ -14,7 +15,8 @@ CREATE TABLE `Felhasznalo` (
   `nev` varchar(100),
   `jelszo` varchar(100),
   `kategoria` integer NOT NULL,
-  `telephely_cim` varchar(255)
+  `telephely_cim` varchar(255),
+  `telefon` varchar(15)
 );
 
 CREATE TABLE `Partnerseg` (
@@ -48,10 +50,11 @@ CREATE TABLE `Rendeles` (
   `id` integer PRIMARY KEY,
   `partnerseg` integer NOT NULL,
   `datum` date NOT NULL,
-  `status` varchar(10) NOT NULL
+  `status` varchar(10) NOT NULL,
+  `sz_cim` integer NOT NULL
 );
 
-CREATE TABLE `Rendeles_tetel` (
+CREATE TABLE `RendelesTetel` (
   `rendeles_id` integer NOT NULL,
   `termek_id` integer NOT NULL,
   `mennyiseg` integer NOT NULL
@@ -67,8 +70,10 @@ ALTER TABLE `Partnerseg` ADD FOREIGN KEY (`vevo`) REFERENCES `Ceg` (`id`);
 
 ALTER TABLE `Termek` ADD FOREIGN KEY (`kategoria`) REFERENCES `Termek_kategoria` (`id`);
 
-ALTER TABLE `Termek` ADD FOREIGN KEY (`id`) REFERENCES `Rendeles_tetel` (`termek_id`);
+ALTER TABLE `Termek` ADD FOREIGN KEY (`id`) REFERENCES `RendelesTetel` (`termek_id`);
 
-ALTER TABLE `Rendeles_tetel` ADD FOREIGN KEY (`rendeles_id`) REFERENCES `Rendeles` (`id`);
+ALTER TABLE `RendelesTetel` ADD FOREIGN KEY (`rendeles_id`) REFERENCES `Rendeles` (`id`);
 
 ALTER TABLE `Rendeles` ADD FOREIGN KEY (`partnerseg`) REFERENCES `Partnerseg` (`id`);
+
+ALTER TABLE `Rendeles` ADD FOREIGN KEY (`sz_cim`) REFERENCES `Felhasznalo` (`id`);
